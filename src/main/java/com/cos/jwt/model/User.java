f4d2c00f654955b7f07af2d8ebb46d241567598b
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-20 18:18:59 +0900

    User Model 생성

---
+++ src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,34 @@
+package com.cos.jwt.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    
+    // 실제로는 테이블 따로 분리하기
+    private String roles; // USER, ADMIN
+
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+
+        return new ArrayList<>();
+    }
+}
