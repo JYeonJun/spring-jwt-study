a9edc8d1b7ddb659c352f33b91d999fd1a820ac0
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-02-28 13:10:47 +0900

    JWT 토큰 생성 및 응답

--- src/main/java/com/cos/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.cos.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.config.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Date;
 
 // 스프링 시큐리티에 UsernamePasswordAuthenticationFilter 존재
 // login 요청시 username과 password를 전송하면 (post)
@@ -66,8 +69,43 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     // attemptAuthentication 실행 후 인증이 정상적으로 수행되면 successfulAuthentication 함수가 실행된다.
     // JWT 토큰을 생성해  request 요청한 사용자에게 JWT 토큰을 response 해주면 된다.
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
         System.out.println("successfulAuthentication 실행됨: 인증 완료!!");
-        super.successfulAuthentication(request, response, chain, authResult);
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        // build.gradle 파일 - implementation group: 'com.auth0', name: 'java-jwt', version: '4.2.1'
+        /*String jwtToken = JWT.create()
+                .withSubject(principalDetails.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET));*/
+
+        // RSA 방식이 아닌 Hash 암호화 방식
+        String jwtToken = JWT.create()
+                .withSubject("cos 토큰")
+                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 10))) // 토큰 만료시간은 10분
+                .withClaim("id", principalDetails.getUser().getId()) // withClaim에는 넣고 싶은 key + value 값
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512("cos")); // 개인키를 가지고 있어야 한다.
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);
+
+        // 유저네임, 패스워드 로그인 정상
+
+        // 서버쪽 세션ID 생성
+        // 클라이언트 쿠키 세션ID 응답
+
+        // 요청할 때마다 쿠키값 세션ID를 항상 들고 서버쪽으로 요청하기 때문에
+        // 서버는 세션ID가 유효한지 판단해 유효하면 인증이 필요한 페이지로 접근하게 하면 된다.
+
+        //=================================================================
+        // 유저네임, 패스워드 로그인 정상
+        // JWT 토큰 생성
+        // 클라이언트로 JWT 토큰 응답
+        // 요청할 때마다 JWT 토큰을 가지고 요청해야 한다.
+        // 서버는 JWT 토큰이 유효한지 판단해야 한다. (필터 생성 필요)
     }
 }
